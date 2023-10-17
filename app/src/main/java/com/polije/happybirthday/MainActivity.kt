8703c6a54dd2f4c793cd35317db797bbf7229a6b
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-18 06:04:01 +0700

    add image

--- app/src/main/java/com/polije/happybirthday/MainActivity.kt
+++ app/src/main/java/com/polije/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,11 +16,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.polije.happybirthday.ui.theme.HappyBirthdayTheme
+import androidx.compose.foundation.Image
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,11 +62,20 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+        )
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Brother!", from = "From Muhyi")
+        GreetingImage(message = "Happy Birthday Brother!", from = "From Muhyi")
     }
 }
